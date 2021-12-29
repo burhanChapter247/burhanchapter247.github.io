1b96eb418c39726e8dbc3597fb64e7d540237408
burhanChpater247 <burhanuddin.s@chapter247.com>
2021-12-29 18:19:37 +0530

    working on fileRead with js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function () {
-  const RaffleList = ["raffle1", "raffle2", "raffle3", "raffle4", "raffle5"];
+  /*  const RaffleList = ["raffle1", "raffle2", "raffle3", "raffle4", "raffle5"];
 
   const selectRaffle = document.getElementById("selectRaffle");
   let option = document.createElement("option");
@@ -7,5 +7,12 @@
   for (const raffle of RaffleList) {
     option.text = raffle;
     selectRaffle.add(option);
-  }
+  } */
+  // As with JSON, use the Fetch API & ES6
+  fetch("txId.txt")
+    .then((response) => response.text())
+    .then((data) => {
+      // Do something with your data
+      console.log(data);
+    });
 })();
