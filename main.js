13a495056c3a79cbe2b1e6ee5be4cbf2fa174977
archana039 <archana.j@chapter247.com>
2021-12-30 11:45:27 +0530

    feat: trying to read s3 bucket file

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 (function () {
-  /*  const RaffleList = ["raffle1", "raffle2", "raffle3", "raffle4", "raffle5"];
 
   const selectRaffle = document.getElementById("selectRaffle");
-  let option = document.createElement("option");
-
-  for (const raffle of RaffleList) {
-    option.text = raffle;
-    selectRaffle.add(option);
-  } */
   // As with JSON, use the Fetch API & ES6
-  fetch("gameIdList.txt")
+  fetch("https://ugoflipbucket.s3.eu-west-2.amazonaws.com/1e8bf697d0fc3dad576281d8d1f2692cfdccc51f11d1759d5ffabfd4e280fe9c.btx")
     .then((response) => response.text())
     .then((data) => {
+      console.log(data,'data+++++++++++')
       // Do something with your data
       let raffleList = data.split(/\n/)
       console.log(raffleList)
