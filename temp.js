eae4d0c2b07db65564af7cd91419fe27b6b2c14c
burhanChpater247 <burhanuddin.s@chapter247.com>
2021-12-31 15:41:55 +0530

    working on reward info

--- temp.js
+++ temp.js
@@ -3,10 +3,10 @@ const getWinnerInfo = (gameId = "61ce92d971d71f359ba8781f") => {
   winnerInfoElement.value = "Loading........";
   fetch(`https://ugoflip.herokuapp.com/v1/raffle/${gameId}/reward-info`)
     .then((response) => response.json())
-    .then((rewards) => {
-      if (rewards) {
+    .then((responseData) => {
+      if (responseData.data) {
         let innerElement = "<ul>";
-        for (const reward of rewards) {
+        for (const reward of responseData.data) {
           if (reward.winningTicketIds.length) {
             innerElement = `<li>Reward: ${reward.rewardTitle}, Price: ${
               reward.rewardPrice
