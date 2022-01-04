84b0fb8471e32b32d423f4064c6d04e0cfe6dde2
archana039 <archana.j@chapter247.com>
2022-01-04 11:04:28 +0530

    fix:use RNG class in main.js file

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const crypto = window.CryptoJS;
 let currentSeed = ""
 const int32OffsetsIn256Bits = [0, 4, 8, 12, 16, 20, 24, 28];
 const int32MaxValue = 0b01111111111111111111111111111111; // equals 2147483647
-import { RNG } from "./rng";
 (function () {
 
   const selectRaffle = document.getElementById("selectGame");
@@ -24,10 +23,6 @@ import { RNG } from "./rng";
 })();
 
 function handleValidate() {
-  // test()
-  // console.log(window.RNG,'RNG++++++++++++')
-  const a = new RNG("aa","aaa")
-  const bsv = window.bsvjs
   const e = document.getElementById("selectGame");
   const raffleId = e.options[e.selectedIndex].value;
   const S3BucketBaseUrl = "https://ugoflipbucket.s3.eu-west-2.amazonaws.com"
@@ -393,4 +388,63 @@ function getNextUInt32Between(o) {
   const result = o.min + m;
   console.log(result, 'result++++++++++getNextUInt32Between')
   return result;
+}
+
+class RNG {
+  constructor(seed, ...moreSeeds) {
+      this.currentSeed = crypto.algo.HMAC.create(CryptoJS.algo.SHA256, "Secret Passphrase");
+      currentSeed.update(Buffer.concat([
+          Buffer.from(seed.toString()),
+          ...moreSeeds.map((s) => Buffer.from(s.toString())),
+      ]));
+  }
+
+  getNext() {
+      console.log(currentSeed, 'getNext+++++++++++')
+      this.currentSeed = crypto
+          .algo.HMAC.create(CryptoJS.algo.SHA256, "Secret Passphrase")
+          .update(this.currentSeed)
+      return currentSeed;
+  }
+
+  getNextUInt32(o) {
+      console.log(o, 'getNextUInt32')
+      if (o) {
+          return getNextUInt32Between(o);
+      }
+
+      const sha256Hash = getNext();
+      console.log(sha256Hash, "sha256HashgetNextUInt32")
+      const numbers = int32OffsetsIn256Bits.map((offset) =>
+          sha256Hash.readUInt32BE(offset)
+      );
+      let result = numbers[0];
+      console.log(result, 'result+++++++++++++')
+      for (let i = 1; i < numbers.length; i++) result = result ^ numbers[i];
+      result = result & int32MaxValue; // this will remove the sign from the result (-42 becomes 42)
+      console.log("getNextUInt32result", result)
+      return result;
+  }
+
+  getNextUInt32Between(o) {
+      console.log(o, 'getNextUInt32BetweengetNextUInt32Between')
+      if (!o) throw new Error("no integer limits provided");
+      if (!o.max) throw new Error("no integer max limit provided");
+      if (!o.min) o.min = 0;
+      o.min = Math.floor(o.min);
+      o.max = Math.floor(o.max);
+      if (o.min < 0) throw new Error(`min limit cannot be smaller than 0`);
+      if (o.min >= o.max)
+          throw new Error(
+              `max limit (${o.max}) must be greater than min limit (${o.min})`
+          );
+
+      const diff = o.max - o.min;
+      const int = getNextUInt32();
+      console.log(int, 'intintintintintintintintintintint')
+      const m = int % diff;
+      const result = o.min + m;
+      console.log(result, 'result++++++++++getNextUInt32Between')
+      return result;
+  }
 }
