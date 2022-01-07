63170c3f70c927e3182525ccd34ddef6d4ba8af1
archana039 <archana.j@chapter247.com>
2022-01-07 15:03:30 +0530

    feat: created seprate function for finalization transaction

--- winnerSelection.js
+++ winnerSelection.js
@@ -4,7 +4,6 @@ function validateInitTransaction(transactionData, pubKey) {
 		signature,
 		messageParts: [messageBuf],
 	} = parseTransaction(transactionData, 1)
-
 	if (messageType !== 0) {
 		alert("Data are corrupted")
 		console.log("Initialization TX message type must be RAFFLE_INITIALIZATION");
@@ -70,6 +69,71 @@ function validateInitTransaction(transactionData, pubKey) {
 		console.log("Game must contain valid additional seeds")
 		removeLoading()
 		return
+
 	}
    return initObject
+}
+
+
+function validateEndTransaction (transactionData, pubKey){
+	const {
+		messageType,
+		signature,
+		messageParts: [messageBuf],
+	} = parseTransaction(transactionData, 1)
+	if (messageType !== 2) {
+		alert("Data are corrupted")
+		console.log("Finalization TX message type must be RAFFLE_FINALIZING");
+		removeLoading()
+	}
+	if (!validateSignature(pubKey, signature, [messageBuf])) {
+		alert("Data are corrupted")
+		console.log("Finalization TX Signature validation failed");
+	}
+	endObject = JSON.parse(messageBuf.toString());
+	const initTxid = Buffer.from(endObject.initializationTxid, "hex");
+	console.log(realInitTxid, 'realInitTxid', initTxid)
+	if (!initTxid.equals(realInitTxid)) {
+		alert("Data are corrupted")
+		console.log(
+			"The Finalization transaction specifies the wrong initialization TXID"
+		);
+		removeLoading()
+		return
+
+	}
+	if (!endObject.lastTicketSoldTimestamp) {
+		alert("Data are corrupted")
+		console.log("Raffle doesn't have last ticket timestamp")
+		removeLoading()
+		return
+
+	}
+	if (!endObject.additionalSeeds.length || !endObject.additionalSeeds[0]) {
+		alert("Data are corrupted")
+		console.log("Invalid additional seeds")
+		removeLoading()
+		return
+
+	}
+	if (endObject.additionalSeeds.length !== initObject.additionalSeeds.length) {
+		alert("Data are corrupted")
+		console.log("Expected additional seeds not found")
+		removeLoading()
+		return
+
+	}
+
+	for (let i = 0; i < endObject.additionalSeeds.length; i++) {
+		const seed = endObject.additionalSeeds[i];
+		const regex = initObject.additionalSeeds[i].regexPattern;
+		if (!stringToRegex(regex).test(seed)) {
+			alert("Data are corrupted")
+			console.log("Invalid seeds");
+			removeLoading()
+			return
+
+		}
+	}
+   return endObject
 }
